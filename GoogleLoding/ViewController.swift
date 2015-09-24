84f647496eaa3e0035acee83e55ca489e8575dfa
lianghailiang <821028514@qq.com>
2015-09-24 15:04:57 +0800

    Initial Commit

---
+++ GoogleLoding/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoogleLoding
+//
+//  Created by lianghailiang on 15/9/24.
+//  Copyright © 2015年 jinvovo_ios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
