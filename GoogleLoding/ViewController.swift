1de329bd2daa68d585dbea8d85b2c12874c2c7e7
lianghailiang <lianghailiang@jinvovo.com>
2015-12-14 17:25:39 +0800

    first

--- GoogleLoding/ViewController.swift
+++ GoogleLoding/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var loadingView: UIView!
 
+    let ovalShapeLayer: CAShapeLayer = CAShapeLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        ovalShapeLayer.strokeColor = UIColor.redColor().CGColor
+        ovalShapeLayer.fillColor = UIColor.clearColor().CGColor
+        ovalShapeLayer.lineWidth = 2
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        
+        let ovalRadius = loadingView.frame.size.height/2 * 0.8
 
+        
+        ovalShapeLayer.path = UIBezierPath(ovalInRect: CGRect(x: loadingView.frame.size.width/2 - ovalRadius, y: loadingView.frame.size.height/2 - ovalRadius, width: ovalRadius * 2, height: ovalRadius * 2)).CGPath
+
+        ovalShapeLayer.lineCap = kCALineCapRound
+        ovalShapeLayer.strokeEnd = 0.8
+        loadingView.layer.addSublayer(ovalShapeLayer)
+        
+        print("123")
+
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        beginSimpleAnimation()
+    }
+    
+    func beginSimpleAnimation() {
+        let rotate = CABasicAnimation(keyPath: "transform.rotation")
+        rotate.duration = 1.5
+        rotate.fromValue = 0
+        rotate.toValue = 2 * M_PI
+        rotate.repeatCount = HUGE
+        loadingView.layer.addAnimation(rotate, forKey: nil)
+    }
 
 }
 
